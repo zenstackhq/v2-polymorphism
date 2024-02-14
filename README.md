ca0074dc293486260c1779fe069a98fbd22e9086
ymc9 <104139426+ymc9@users.noreply.github.com>
2024-02-14 21:58:39 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ZenStack Polymorphic Relations Sample
+
+See [this documentation](https://zenstack.dev/docs/next/guides/polymorphism) for more details.
---
+++ script.ts
@@ -0,0 +1,62 @@
+import { PrismaClient } from '@prisma/client';
+import { enhance } from '@zenstackhq/runtime';
+import { inspect } from 'util';
+
+const prisma = new PrismaClient({ log: ['info'] });
+
+async function main() {
+    await prisma.user.deleteMany();
+    await prisma.content.deleteMany();
+
+    const user = await prisma.user.create({ data: { id: 1 } });
+
+    const db = enhance(
+        prisma,
+        { user: { id: user.id } },
+        {
+            kinds: ['delegate', 'policy'],
+        }
+    );
+
+    const post = await db.post.create({
+        data: { owner: { connect: { id: user.id } }, title: 'Post1' },
+    });
+    console.log('Post created:', inspect(post));
+
+    const video = await db.video.create({
+        data: {
+            owner: { connect: { id: user.id } },
+            name: 'Video1',
+            duration: 100,
+        },
+    });
+    console.log('Video created:', inspect(video));
+
+    console.log('All content:', inspect(await db.content.findMany()));
+    console.log('All posts:', inspect(await db.post.findMany()));
+    console.log('All videos:', inspect(await db.video.findMany()));
+
+    await db.user.update({
+        where: { id: user.id },
+        data: {
+            contents: { updateMany: { where: {}, data: { published: true } } },
+        },
+    });
+    console.log(
+        'All content after publish:',
+        inspect(await db.content.findMany())
+    );
+
+    await db.content.deleteMany();
+    console.log('All posts after delete:', inspect(await db.post.findMany()));
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
